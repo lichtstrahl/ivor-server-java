2c8590f5a5201bd72c0c158dda0b6ce2cfa79274
Игорь Смирнов <cool.rainbow2012@yandex.ru>
2019-01-31 17:57:34 +0300

    Добавил Фильтр для POST запросов и настроил Tomcat для GET запросов в UTF-8

---
+++ src/main/java/root/id/util/UTF8Filter.java
@@ -0,0 +1,27 @@
+package root.id.util;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class UTF8Filter implements Filter {
+    private String encoding = "utf-8";
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        String encodingParam = filterConfig.getInitParameter("encoding");
+        if (encodingParam != null) {
+            encoding = encodingParam;
+        }
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        servletRequest.setCharacterEncoding(encoding);
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+        // nothing todo
+    }
+}
