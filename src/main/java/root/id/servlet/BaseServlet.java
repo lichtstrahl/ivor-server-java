8c3bf968f19cffe67f94faf51b5cab167fea213c
Igor Smirnov <igor.smirnov@deltasolutions.ru>
2019-02-04 12:47:29 +0300

    Добавил обновление подключения

--- src/main/java/root/id/servlet/BaseServlet.java
+++ src/main/java/root/id/servlet/BaseServlet.java
@@ -5,7 +5,6 @@ import com.google.gson.GsonBuilder;
 import root.id.dto.ServerAnswer;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
